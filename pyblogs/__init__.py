c3406760134f6a3c31f7541f41ee77bc2914894f
Awet <awetkebedom@gmail.com>
2019-09-10 22:44:36 +0300

    added some modifications
    Signed-off-by: Awet <awetkebedom@gmail.com>

--- pyblogs/__init__.py
+++ pyblogs/__init__.py
@@ -1,6 +1,17 @@
-
 import flask
+import flask_sqlalchemy
+import os
+import flask_migrate
 
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'my-secret-key'
+
+# create a database object
+
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///'+os.path.join(os.path.dirname(__file__),'pyblogs.db')
+
+db = flask_sqlalchemy.SQLAlchemy(app)
+
+
+
 from pyblogs import routes
